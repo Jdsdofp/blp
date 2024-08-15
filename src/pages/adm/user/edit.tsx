8383918e55b990b525ab573be3f912ad0a23892c
Jds Dofp <jdsdofp@gmail.com>
2024-08-15 14:31:56 -0300

    criando o modulo editar usuario

--- src/pages/adm/user/edit.tsx
+++ src/pages/adm/user/edit.tsx
@@ -1,11 +1,17 @@
 import { Edit } from "@refinedev/antd";
+import { Form, Input } from "antd";
 
 
 
 export const AdmUserEdit = () =>{
+
     return (
         <Edit title="Editar Usuário" breadcrumb>
-            
+            <Form>
+                <Form.Item>
+                    <Input/>
+                </Form.Item>
+            </Form>
         </Edit>
     )
 };
