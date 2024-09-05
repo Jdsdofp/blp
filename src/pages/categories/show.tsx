bced6c14a5f993a28393739180e3529a9a2137b7
Jds Dofp <jdsdofp@gmail.com>
2024-09-05 17:59:44 -0300

    aiajh

--- src/pages/categories/show.tsx
+++ src/pages/categories/show.tsx
@@ -1,29 +1,55 @@
-import { Show, TextField } from "@refinedev/antd";
-import { useShow } from "@refinedev/core";
-import { Typography } from "antd";
-import { useParams } from "react-router-dom";
+  import { Show, TextField } from "@refinedev/antd";
+  import { useList, useShow } from "@refinedev/core";
+  import { List, Typography, Card, Row, Col } from "antd";
+  import { useParams } from "react-router-dom";
 
-const { Title } = Typography;
+  const { Title } = Typography;
 
-export const DocumentShow = () => {
+  export const DocumentShow = () => {
+    const queryParams = new URLSearchParams(location.search);
+    const status = queryParams.get("status");
+    const filialId = queryParams.get("filialId");
 
-  const queryParams = new URLSearchParams(location.search);
-  const status = queryParams.get("status");
-  const filialId = queryParams.get("filialId");
+    const { data } = useList({
+      resource: 'document',
+      meta: { endpoint: `listar-documentos-status-filial/${status}/${filialId}` }
+    });
 
-  
+    const record = data?.data;
 
-  const { queryResult } = useShow({});
-  const { data, isLoading } = queryResult;
+    return (
+      <Show>
+        <List
+          
+          dataSource={record}
+          
+          renderItem={item => (
+            <>
+              
 
-  const record = data?.data;
+            <Row gutter={16} align={"top"}>
+            <Col span={8}>
+            <Card
+                size="small"
+                title={[`${item.d_num_protocolo}`]}
+                style={{ width: 300, margin: '16px', color: item.d_situacao == 'Vencido' ? 'red' : 'Highlight'}}
+                bordered
+                cover
+              >
+                <p>Status: {item.d_situacao}</p>
+                <p>Filial: {item.filiais.f_nome}</p>
+                <p>Descrição: {item.tipo_documentos.td_desc}</p>
+              </Card>
+             </Col>
+            </Row>
+            
+            </>
+          )}
+        />
 
-  return (
-    <Show>
-      <Title level={5}>{"ID"}</Title>
-      <TextField value={filialId} />
-      <Title level={5}>{"Title"}</Title>
-      <TextField value={status} />
-    </Show>
-  );
-};
+      </Show>
+
+
+
+    );
+  };
